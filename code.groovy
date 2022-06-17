de7e470de5cc035d431f1072ba48134737752dd9
ChaitanyaChandra <chay@outlook.in>
2022-06-17 09:27:52 +0530

    automated commit #626

--- code.groovy
+++ code.groovy
@@ -18,3 +18,29 @@ pipelineJob('ansible') {
     }
   }
 }
+
+folder('CI-Pipelines') {
+  displayName('CI-Pipelines')
+  description('CI-Pipelines')
+}
+
+pipelineJob('CI-Pipelines/nodejs') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/ChaitanyaChandra/jenkins.git',)
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('ci')
+      'lightweight'(true)
+    }
+  }
+}
