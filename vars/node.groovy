2c65d01d2e78cdc306a96a1f67575ba04988adbe
ChaitanyaChandra <chay@outlook.in>
2022-06-17 10:04:26 +0530

    automated commit #631

--- vars/node.groovy
+++ vars/node.groovy
@@ -1,7 +1,38 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+pipeline {
+    agent { label 'work_station'}
+    options {
+        disableConcurrentBuilds()
+        ansiColor('xterm')
+    }
+    triggers { pollSCM('* * * * *') }
+    stages
+            {
+                stage ('compile the code')
+                        {
+                            steps
+                                    {
+                                        sh 'echo compile code'
+                                        git branch: 'master', url: 'https://github.com/ChaitanyaChandra/spec.git'
+                                        dir('spec') {
+                                        }
+                                    }
+                        }
 
-def warning(message) {
-    echo "WARNING: ${message}"
+                stage ('check the code quality')
+                        {
+                            steps
+                                    {
+                                        sh 'echo check code quality'
+                                    }
+                        }
+
+                stage ('Test cases')
+                        {
+                            steps
+                                    {
+                                        sh 'echo Test cases'
+                                    }
+                        }
+
+            }
 }
