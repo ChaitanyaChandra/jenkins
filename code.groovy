2eed2f96e4619ad3388169c001e64ab5833b722b
ChaitanyaChandra <chay@outlook.in>
2022-06-17 09:11:36 +0530

    automated commit #625

--- code.groovy
+++ code.groovy
@@ -18,34 +18,3 @@ pipelineJob('ansible') {
     }
   }
 }
-
-folder('CI-Pipelines') {
-  displayName('CI-Pipelines')
-  description('CI-Pipelines')
-}
-
-def app = ["mongodb", "redis", "rabbitmq", "mysql", "catalogue", "user", "cart", "shipping", "payment", "dispatch",
-       "frontend"]
-def i = 0
-while (i < app.size){
-  pipelineJob("CI-Pipelines/${app[i]}") {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'("https://github.com/MajorChowdary/project-${app[i]}.git",)
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('jenkins')
-        'lightweight'(true)
-      }
-    }
-  }
-}
