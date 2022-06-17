ce4a0be9f9a37a61113db76d4308c9906dd2620b
ChaitanyaChandra <chay@outlook.in>
2022-06-17 10:06:03 +0530

    automated commit #632

--- vars/node.groovy
+++ vars/node.groovy
@@ -1,38 +1,40 @@
-pipeline {
-    agent { label 'work_station'}
-    options {
-        disableConcurrentBuilds()
-        ansiColor('xterm')
-    }
-    triggers { pollSCM('* * * * *') }
-    stages
-            {
-                stage ('compile the code')
-                        {
-                            steps
-                                    {
-                                        sh 'echo compile code'
-                                        git branch: 'master', url: 'https://github.com/ChaitanyaChandra/spec.git'
-                                        dir('spec') {
+def call() {
+    pipeline {
+        agent { label 'work_station'}
+        options {
+            disableConcurrentBuilds()
+            ansiColor('xterm')
+        }
+        triggers { pollSCM('* * * * *') }
+        stages
+                {
+                    stage ('compile the code')
+                            {
+                                steps
+                                        {
+                                            sh 'echo compile code'
+                                            git branch: 'master', url: 'https://github.com/ChaitanyaChandra/spec.git'
+                                            dir('spec') {
+                                            }
                                         }
-                                    }
-                        }
+                            }
 
-                stage ('check the code quality')
-                        {
-                            steps
-                                    {
-                                        sh 'echo check code quality'
-                                    }
-                        }
+                    stage ('check the code quality')
+                            {
+                                steps
+                                        {
+                                            sh 'echo check code quality'
+                                        }
+                            }
 
-                stage ('Test cases')
-                        {
-                            steps
-                                    {
-                                        sh 'echo Test cases'
-                                    }
-                        }
+                    stage ('Test cases')
+                            {
+                                steps
+                                        {
+                                            sh 'echo Test cases'
+                                        }
+                            }
 
-            }
+                }
+    }
 }
