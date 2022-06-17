e39908fd9a23fcef7c78b529dd2800b1489ed586
ChaitanyaChandra <chay@outlook.in>
2022-06-17 11:08:52 +0530

    automated commit #648

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,3 @@
 def SonarQube() {
-    sh "sonar-scanner -Dsonar.projectKey=nodejs -Dsonar.sources=. -Dsonar.host.url=http://sonar-dev.chaitu.org:9000 -Dsonar.password=@123Chaitu"
+    sh "sonar-scanner -Dsonar.projectKey=nodejs -Dsonar.sources=. -Dsonar.host.url=http://sonar-dev.chaitu.org:9000 -Dsonar.login=admin -Dsonar.password=@123Chaitu"
 }
