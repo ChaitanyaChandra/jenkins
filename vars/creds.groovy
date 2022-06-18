3648e5f030905efd9a1415ac108ca88e619a6959
ChaitanyaChandra <chay@outlook.in>
2022-06-18 11:40:27 +0530

    automated commit #655

--- vars/creds.groovy
+++ vars/creds.groovy
@@ -2,21 +2,23 @@ import jenkins.*
 import jenkins.model.*
 import hudson.*
 import hudson.model.*
-def jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-        com.cloudbees.plugins.credentials.Credentials.class,
-        Jenkins.instance,
-        null,
-        null
-);
-
+class Globals {
+    static List<String> jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+            com.cloudbees.plugins.credentials.Credentials.class,
+            Jenkins.instance,
+            null,
+            null
+    );
+}
 def call(ID){
-    for (creds in jenkinsCredentials) {
-        if(creds.id == $ID){
-          //  println(creds.username)
-          //  println(creds.password)
+    for (creds in Globals.jenkinsCredentials) {
+        if(creds.id == ID){
+            //  println(creds.username)
+            //  println(creds.password)
             if (ID == 'SONAR'){
                 common.SonarQube(creds.username, creds.password)
             }
         }
     }
 }
+call('SONAR')
