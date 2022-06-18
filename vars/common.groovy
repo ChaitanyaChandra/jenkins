c112f624c48d5ee003b708c91f64b9c4c0627e95
ChaitanyaChandra <chay@outlook.in>
2022-06-18 12:39:55 +0530

    automated commit #659

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,4 +1,4 @@
-def SonarQube(USER, PASSWORD) {
-    sh "sonar-scanner -Dsonar.projectKey=node -Dsonar.sources=. -Dsonar.host.url=http://sonar-dev.chaitu.org:9000 -Dsonar.login=${USER} -Dsonar.password=${PASSWORD}"
-    sh "sonar-quality-gate.sh ${USER} ${PASSWORD} sonar-dev.chaitu.org nodejs"
+def SonarQube(USER, PASSWORD, URL, PROJECT) {
+    sh "sonar-scanner -Dsonar.projectKey=${PROJECT} -Dsonar.sources=. -Dsonar.host.url=http://${URL}:9000 -Dsonar.login=${USER} -Dsonar.password=${PASSWORD}"
+    sh "sonar-quality-gate.sh ${USER} ${PASSWORD} ${URL} ${PROJECT}"
 }
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -27,7 +27,7 @@ def call() {
                                 steps
                                         {
                                             script{
-                                                common.SonarQube(SONAR_CREDS_USR, SONAR_CREDS_PSW)
+                                                common.SonarQube(SONAR_CREDS_USR, SONAR_CREDS_PSW, "sonar-dev.chaitu.org",  "nodejs")
                                             }
                                         }
                             }
