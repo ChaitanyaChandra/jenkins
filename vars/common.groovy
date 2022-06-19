583f36201c1743ea7f0e96fb4cd1a6a939cc24fa
ChaitanyaChandra <chay@outlook.in>
2022-06-19 11:23:42 +0530

    automated commit #675

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,12 +1,13 @@
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 def SonarQube(USER, PASSWORD, URL, PROJECT) {
     sh "sonar-scanner -Dsonar.projectKey=${PROJECT} -Dsonar.sources=. -Dsonar.host.url=http://${URL}:9000 -Dsonar.login=${USER} -Dsonar.password=${PASSWORD}"
     sh "sonar-quality-gate.sh ${USER} ${PASSWORD} ${URL} ${PROJECT}"
 }
 
 def publish_artifacts(){
-    if (env.GIT_BRANCH=="tag"){
+    if (env.GIT_BRANCH=="*tag*"){
         println "run with tag"
     }
     else
-        println "run with branch"
+        Utils.markStageSkippedForConditional('Publish Artifacts')
 }
