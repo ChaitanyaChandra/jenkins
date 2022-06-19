0eb611bcc25ab777b0289d0b77691b32c2c0a9d8
ChaitanyaChandra <chay@outlook.in>
2022-06-19 21:15:58 +0530

    automated commit #684

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,17 +4,25 @@ def call() {
         options {
             disableConcurrentBuilds()
             ansiColor('xterm')
-            addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${gitTag}"
         }
         environment {
             SONAR_CREDS = credentials('SONAR')
-
         }
 //        triggers {
 //            pollSCM('H/2 * * * *')
 //        }
         stages
                 {
+                    stage('Label Builds')
+                            {
+                                steps {
+                                    script {
+                                        env.gitTag = GIT_BRANCH.split('/').last()
+                                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${gitTag}"
+                                    }
+                                }
+                            }
+
                     stage ('compile the code')
                             {
                                 steps
