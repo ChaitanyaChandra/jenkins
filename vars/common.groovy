72c3d5cfb6da5ff8d9e814b6593d2b841f46b94e
ChaitanyaChandra <chay@outlook.in>
2022-06-20 13:56:23 +0530

    automated commit #740

--- vars/common.groovy
+++ vars/common.groovy
@@ -7,7 +7,7 @@ def prepare_artifacts(){
     println "publishing artifacts"
     sh '''
       npm install 
-      zip -r nodejs-${gitTag}.zip node_modules index.js views public
+      zip -r nodejs-${gitTag}.zip node_modules index.js views public package.json
     '''
 }
 
