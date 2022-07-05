6f7e6d02dc4792895a62991cbae251e4b7c6a40a
ChaitanyaChandra <chay@outlook.in>
2022-07-05 14:39:54 +0530

    automated commit #1189

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,6 @@
 def SonarQube(USER, PASSWORD, URL, PROJECT) {
-//    sh "sonar-scanner -Dsonar.projectKey=${PROJECT} -Dsonar.sources=. -Dsonar.host.url=http://${URL}:9000 -Dsonar.login=${USER} -Dsonar.password=${PASSWORD}"
-//    sh "sonar-quality-gate.sh ${USER} ${PASSWORD} ${URL} ${PROJECT}"
+    sh "sonar-scanner -Dsonar.projectKey=${PROJECT} -Dsonar.sources=. -Dsonar.host.url=http://${URL}:9000 -Dsonar.login=${USER} -Dsonar.password=${PASSWORD}"
+    sh "sonar-quality-gate.sh ${USER} ${PASSWORD} ${URL} ${PROJECT}"
 }
 
 def prepare_artifacts(){
