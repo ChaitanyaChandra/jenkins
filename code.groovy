865e21ca01f25f6d2d226c9ec1ce35f3721026d6
ChaitanyaChandra <chay@outlook.in>
2022-07-05 21:53:45 +0530

    automated commit #1263

--- code.groovy
+++ code.groovy
@@ -49,6 +49,33 @@ pipelineJob('CI-Pipelines/nodejs') {
   }
 }
 
+
+pipelineJob('CI-Pipelines/frontend') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/ChaitanyaChandra/frontend.git',)
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/tags/*')
+          }
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+
+  }
+}
+
+
 folder('mutable') {
   displayName('mutable')
   description('mutable')
---
+++ vars/frontend.groovy
@@ -0,0 +1,67 @@
+def call() {
+    pipeline {
+        agent { label 'work_station'}
+        options {
+            disableConcurrentBuilds()
+            ansiColor('xterm')
+        }
+        environment {
+            CREDS = credentials('APP_CREDS')
+        }
+//        triggers {
+//            pollSCM('H/2 * * * *')
+//        }
+        stages
+                {
+                    stage('Label Builds')
+                            {
+                                steps {
+                                    script {
+                                        env.gitTag = GIT_BRANCH.split('/').last()
+                                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${gitTag}"
+                                    }
+                                }
+                            }
+
+                    stage ('compile the code')
+                            {
+                                steps
+                                        {
+                                            sh 'echo compile code'
+                                            git branch: 'feature', url: 'https://github.com/ChaitanyaChandra/frontend.git'
+                                            dir('frontend') {
+                                            }
+                                        }
+                            }
+
+                    stage ('Publish artifacts')
+                            {
+                                when {
+                                    expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                                }
+                                steps
+                                        {
+                                            script{
+                                                common.prepare_artifacts("frontend")
+                                                common.publish_artifacts(CREDS_USR, CREDS_PSW, "nexus-dev.chaitu.org", "nginx", "frontend")
+                                            }
+                                        }
+                            }
+                    stage('Publish AMI') {
+                        when {
+                            expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+                        }
+                        steps {
+                            script {
+                                common.make_AMI(CREDS_USR, CREDS_PSW, "nginx", "frontend", "dev")
+                            }
+                        }
+                    }
+                }
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
