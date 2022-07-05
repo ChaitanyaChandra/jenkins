36655cdd27063d73c0c73ad7615b2362db5b5e5d
ChaitanyaChandra <chay@outlook.in>
2022-07-05 18:58:06 +0530

    automated commit #1236

--- vars/common.groovy
+++ vars/common.groovy
@@ -12,13 +12,13 @@ def prepare_artifacts(){
 }
 
 def make_AMI(USER, PASSWORD, PROJECT) {
-    sh '''
+    sh """"
     terraform init 
     terraform plan -var APP_VERSION=${gitTag} -var PROJECT="nodejs" -var ENV="dev" -var NEXUS_USERNAME=${USER} -var NEXUS_PASSWORD=${PASSWORD} -var PROJECT=${PROJECT}
     terraform apply -auto-approve -var APP_VERSION=${gitTag} -var PROJECT=nodejs -var ENV=dev -var NEXUS_USERNAME=${USER} -var NEXUS_PASSWORD=${PASSWORD} -var PROJECT=${PROJECT}
     terraform state rm module.${COMPONENT}-ami.aws_ami_from_instance.ami
     terraform destroy -auto-approve -var APP_VERSION=${gitTag} -var PROJECT=nodejs -var ENV=dev -var NEXUS_USERNAME=${USER} -var NEXUS_PASSWORD=${PASSWORD} -var PROJECT=${PROJECT}
-  '''
+  """
 }
 
 def publish_artifacts(USER, PASSWORD, URL, PROJECT){
