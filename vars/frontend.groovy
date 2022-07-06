cb490a308549cb4edf70b647e991b7524cbe68fd
ChaitanyaChandra <chay@outlook.in>
2022-07-06 15:58:58 +0530

    automated commit #1301

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -8,6 +8,9 @@ def call() {
         environment {
             CREDS = credentials('APP_CREDS')
         }
+        parameters {
+            choice(name: 'version', choices: [], description: 'Pick Environment')
+        }
 //        triggers {
 //            pollSCM('H/2 * * * *')
 //        }
